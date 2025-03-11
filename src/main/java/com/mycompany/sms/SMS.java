a79228fc768eca410286277bdbc670e1fceebe65
Luis Pogi <Luis Pogi@POGICHRISTIAN>
2025-03-11 19:54:40 +0800

    ADMINLOGIN

---
+++ src/main/java/com/mycompany/sms/SMS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sms;
+
+/**
+ *
+ * @author Luis Pogi
+ */
+public class SMS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
